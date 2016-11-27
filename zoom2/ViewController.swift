e22c6ba6ca1d2d89a0897c54578c2363c5a68477
Maria Susana Moline Venanzi <susana@Marias-MacBook-Air.local>
2016-11-26 20:35:12 -0600

    Esto es el comit con el zoom para imágenes

---
+++ zoom2/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  zoom2
+//
+//  Created by Maria Susana Moline Venanzi on 11/22/16.
+//  Copyright © 2016 Uberbliss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UIScrollViewDelegate
+{
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 6.0
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+        
+    }
+    
+}
